e3d55403942876344577066580a4ec408ccec9f7
Albert Solana <albertsolana@MacBook-de-Clara.local>
2019-07-05 17:00:21 +0200

    Added a TODO list inside each API call

--- eidas_bridge.py
+++ eidas_bridge.py
@@ -16,6 +16,14 @@ def eidas_link_did(did, certificate, proof) -> str:
     util.check_args(certificate, bytes)
     util.check_args(proof, bytes)
 
+    # TO DO 
+    '''
+    - construct a json structure with the given parameters and return it
+    - check that the proof really correspond to a PKCS#1 signature of
+        the DID passed signed with the privkey of the certificate
+    '''
+    
+
     return ""
 
 def eidas_get_service_endpoint_struct(storage_endpoint) -> str:
@@ -25,6 +33,12 @@ def eidas_get_service_endpoint_struct(storage_endpoint) -> str:
     eIDAS and DID linking information and returns the correspondent JSON
     """
     util.check_args(storage_endpoint, str)
+
+     # TO DO 
+    '''
+    - construct a json structure with the given parameters and return it
+
+    '''
 
     return ""
 
@@ -36,6 +50,12 @@ def eidas_sign_credential(json_credential) -> str:
     """
     util.check_args(json_credential, str)
 
+     # TO DO 
+    '''
+    - construct a json structure with the given parameters and return it
+    - launch an OCSP request to check the validity of the certificate
+    '''
+
     return ""
 
 def eidas_verify_credential(json_credential) -> str:
@@ -45,5 +65,17 @@ def eidas_verify_credential(json_credential) -> str:
     Returns: VALID or NOT VALID
     """
     util.check_args(json_credential, str)
+
+     # TO DO 
+    '''
+    - Retrieve the DID from the credential
+    - Resolve a DID Document from the DID
+    - Get the storage endpoint to get the eIDAS json structure
+    - Check that this DID has an eIDAS certificate (with a public key) 
+        that can verify the proof stored
+    - Retrieve the eIDAS json block structure from the credential
+    - Check the validity of the certificate when the credential was issued
+    - return the correspondent value
+    '''
 
     return "NOT VALID"
