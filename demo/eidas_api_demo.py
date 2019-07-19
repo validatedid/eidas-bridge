b6324abaa4aca64c174647dd5f7b954f2926e422
Albert Solana <albertsolana@MacBook-de-Clara.local>
2019-07-19 16:46:05 +0200

    updated to swagger support

--- demo/eidas_api_demo.py
+++ demo/eidas_api_demo.py
@@ -3,7 +3,7 @@
 
 import json, threading
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restplus import Resource, Api
 from flask_jsonpify import jsonify
 from eidas_demo import init_server, demo_eidas_link_did, demo_eidas_get_service_endpoint, \
     demo_eidas_sign_credential, demo_eidas_verify_credential
@@ -12,29 +12,67 @@ app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 api = Api(app)
 
-@app.route('/eidas/link-did', methods=['GET'])
-def get_eidas_link_did():
-    return jsonify(json.loads(demo_eidas_link_did()))
+name_space = api.namespace('eidas', description="eIDAS DEMO APIs")
 
-@app.route('/eidas/service-endpoint', methods=['GET'])
-def get_eidas_service_endpoint():
-    return jsonify(json.loads(demo_eidas_get_service_endpoint()))
+@name_space.route('/link-did')
+class EIDASLinkDID(Resource):
+    def get(self):
+        """ 
+        Link the Issuer DID with eIDAS certificate
 
-@app.route('/eidas/sign-credential', methods=['GET'])
-def get_eidas_sign_credential():
-    return jsonify(demo_eidas_sign_credential())
+        Receives a DID, an eIDAS certificate, its proof of possession, and 
+        optionally the padding of the signature proof (accepts PKCS#1 and PSS)
 
-@app.route('/eidas/verify-credential', methods=['GET'])
-def get_eidas_verify_credential():
-    server_thread = threading.Thread(target=init_server, daemon=True)
+        Returns the JSON that needs to be stored on the Agent public Storage
+        (i.e: an Identity Hub)
+        """
+        return jsonify(json.loads(demo_eidas_link_did()))
 
-    # launch localhost server
-    server_thread.start()
-    # check if server started
-    if server_thread.is_alive():
-        return jsonify(demo_eidas_verify_credential())
-    else:
-        return "ERROR: Cannot start server"
+@name_space.route('/service-endpoint')
+class EIDASServiceEndpoint(Resource):
+    def get(self):
+        """ 
+        Contructs the JSON structure that needs to be added to the Issuer's 
+        DID Document Service Endpoint Section. 
+
+        Receives a did and a service endpoint where it is stored the issuer's 
+        eIDAS and DID linking information.
+
+        Returns the correspondent JSON to be added to the Service Endpoint 
+        Section of the Issuer's DID Document.
+        """
+        return jsonify(json.loads(demo_eidas_get_service_endpoint()))
+
+@name_space.route('/sign-credential')
+class EIDASSignCredential(Resource):
+    def get(self):
+        """ 
+        Checks the validity of the issuer's eIDAS certificate against a 
+        Trusted Service Provider and adds the corresponde response to the 
+        received credential JSON structure.
+
+        Not Supported at this Phase 0.
+        """
+        return jsonify(demo_eidas_sign_credential())
+
+@name_space.route('/verify-credential')
+class EIDASVerifyCredential(Resource):
+    def get(self):
+        """
+        Verifies that the credential issuer had a valid eIDAS certificate 
+        at the moment of issuing the passed credential.
+
+        Throws EIDASProofException on signarure not valid
+        """
+        server_thread = threading.Thread(target=init_server, daemon=True)
+
+        # launch localhost server
+        server_thread.start()
+        # check if server started
+        if server_thread.is_alive():
+            return jsonify(demo_eidas_verify_credential())
+        else:
+            return "ERROR: Cannot start server"
 
 if __name__ == '__main__':
      app.run(port='5002')
