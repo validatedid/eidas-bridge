90562cce3a747b95e8ba02399a54b65d60182c37
Albert Solana <albertsolana@MacBook-de-Clara.local>
2019-07-22 11:29:05 +0200

    eidas_get_service_endpoint swagger documented

--- demo/eidas_bridge_api.py
+++ demo/eidas_bridge_api.py
@@ -39,19 +39,42 @@ eidas_link_input_model = api.model('EIDASLink_in', {
 })
 
 proof_model = api.model('EIDAS_proof', {
-    'type' : fields.String(description="Signature type", required=True),
-    'padding': fields.String(description="Signature padding: PKCS1-v1_5 or PSS", required=True),
-    'signatureValue' : fields.Raw(description="DID-hashed signature with public key", required=True)
+    'type' : fields.String(
+        description="Signature type", 
+        required=True,
+        example="RsaSignature2018"),
+    'padding': fields.String(
+        description="Signature padding: PKCS1-v1_5 or PSS", 
+        required=True,
+        example="PSS"),
+    'signatureValue' : fields.String(
+        description="DID-hashed signature with public key", 
+        required=True,
+        example="4f1bb7069e1508901e83d9dd71043e35fbc8ecf3077625206dd00cf8f12365096cc1cf07822479e571689bc67c50a7d9ca66c43865e490044729af3356e853073073c11e9fa517f7b35748146c1c1101406f66866969ad5915054e3633ab3c247d6b09be909ece6d018ad309b1b34c45b223227d74928278640e0e6a62de0309309e609e8927eb7abd098dfb8a30e8c91fde3ea4fbe804b2967db2c994d303de1e6ac837cfd2a11414ace2bd75148e917b3505f17fabc4805484164a69fdc1d28122e977c1fa4f62b39a601915d8fe0b1bd6e2932db6c8ca3b2bca3ab04f3aebf83d081122d42248dc2a2f292f2c2bfc42244c3118109ab9f001a85cbdd52f71")
 })
 
 eidas_link_output_model = api.model('EIDASLink_out', {
-    'type': fields.String(description="Structure type: should be EidasLink", required=True),
-    'created': fields.String(description="Structure datetime stamp creation: should be similar to '2002-10-10T17:00:00Z'", required=True),
-    'did': fields.String(description="DID", required=True),
-    'certificate': fields.Raw(description="x509 certificate", required=True),
-    'proof': fields.Nested(proof_model, description="Signature proof structure", required=True)
+    'type': fields.String(
+        description="Structure type: should be EidasLink", 
+        required=True,
+        example="EidasLink"),
+    'created': fields.String(
+        description="Structure datetime stamp creation: should be similar to '2002-10-10T17:00:00Z'", 
+        required=True,
+        example="2019-07-22 09:22:00.688300+00:00"),
+    'did': fields.String(
+        description="DID", 
+        required=True,
+        example="did:sov:55GkHamhTU1ZbTbV2ab9DE"),
+    'certificate': fields.String(
+        description="x509 certificate", 
+        required=True,
+        example="-----BEGIN CERTIFICATE-----\nMIIDYDCCAkigAwIBAgIUI63ffVceaNc1kN9O0q/4jSjbkU0wDQYJKoZIhvcNAQEL\nBQAwXTELMAkGA1UEBhMCRVMxEzARBgNVBAgMClRFU1RfU1RBVEUxEjAQBgNVBAcM\nCVRFU1RfQ0lUWTEQMA4GA1UECgwHQ0FfQUNNRTETMBEGA1UEAwwKbXlzaXRlLmNv\nbTAeFw0xOTA3MDkxNDA3MjBaFw0yMDA3MDgxNDA3MjBaMF0xCzAJBgNVBAYTAkVT\nMRMwEQYDVQQIDApURVNUX1NUQVRFMRIwEAYDVQQHDAlURVNUX0NJVFkxEDAOBgNV\nBAoMB0NBX0FDTUUxEzARBgNVBAMMCm15c2l0ZS5jb20wggEiMA0GCSqGSIb3DQEB\nAQUAA4IBDwAwggEKAoIBAQDm6RhyIeFZHn4bGQ/2UQ+aflczCo3Ej04LJXfiIU1Q\nt7xRq3e+uh7nTLffnS7fj/ZZBBREmR/D/SJBTlxv7WQEbscV/pf2LoZLjoC4M4ye\n43lUHRmWsm4J50tu9zcSheqXCRyAK/Ai6RUBy86NKXMFTUp/ONxS0BxJg8GU03Xd\nXGnYzdmZZXGDnublGYq03gD/cZYguS7/HS8v/MckdmjYPTy2syGL9unYkjWn7vig\niaDc2leAM4agKB6PODJSFla15HLoqskKX1SgtLJUHxu/FOo6hYdCt+GxpV1xhl/r\nEf3/SFeTZrJgL11m5ABDli2zAmCn4bjBNnNcXWy5QV0pAgMBAAGjGDAWMBQGA1Ud\nEQQNMAuCCWxvY2FsaG9zdDANBgkqhkiG9w0BAQsFAAOCAQEAYPUn0TzGyn438++1\nV2jMHC653C8tn3vVF5nTT7Td+ihc+KaaNDYsgyY2JpBIMRwlNgoNU0Da3P/9ZDn3\nlFJElUg8WpWPvpXtbS4udqn6UcfT9mFJtkzKg3CK5i50GRCabV9FPbY1bzYtUbY+\nEntXtI2h0dxcgzgOw6pkXFB3O7ZbbshpqWTlHtTtbxxrOFq0zcpyS92G+NTF6ASS\nhXcIf90du/mBWd2dinF/w2nkRAWfGBy8bGnUSJ93rPVwLjI0PDeHh7+PSQ+3X6mG\n5DI9EmzEC7esW6wJbhgiOYXLavAOmLfI0yq/z8SZMvFYwBE69VuGfPSj/u4nIhA5\nK0Qgnw==\n-----END CERTIFICATE-----\n"),
+    'proof': fields.Nested(
+        proof_model, 
+        description="Signature proof structure", 
+        required=True)
 })
-
 
 @eidas.route('/link-did')
 class EIDASLinkDID(Resource):
@@ -67,7 +90,7 @@ class EIDASLinkDID(Resource):
         Returns the JSON that needs to be stored on the Agent public Storage
         (i.e: an Identity Hub)
         """
-        
+
         return json.loads(
             eidas_link_did(
                 request.json['did'], 
@@ -77,9 +100,37 @@ class EIDASLinkDID(Resource):
             )
         )
 
+eidas_service_input_model = api.model('EIDASService_in', {
+    'did': fields.String(
+        description="DID", 
+        required=True,
+        example="did:example:21tDAKCERh95uGgKbJNHYp"),
+    'service_endpoint': fields.String(
+        description="Service Endpoint", 
+        required=True,
+        example="http://service_endpoint.sample/did:example:21tDAKCERh95uGgKbJNHYp/eidas")
+})
+
+eidas_service_output_model = api.model('EIDASService_out', {
+    'id': fields.String(
+        description="Service Endpoint Identifier", 
+        required=True,
+        example="did:example:21tDAKCERh95uGgKbJNHYp#eidas"),
+    'type': fields.String(
+        description="Service Endpoint type: should be EidasService", 
+        required=True,
+        example="EidasService"),
+    'serviceEndpoint': fields.String(
+        description="Service Endpoint URL", 
+        required=True,
+        example="http://localhost:8000/did:example:21tDAKCERh95uGgKbJNHYp/eidas"),
+})
+
 @eidas.route('/service-endpoint')
 class EIDASServiceEndpoint(Resource):
-    def get(self):
+    @eidas.marshal_with(eidas_service_output_model)
+    @eidas.expect(eidas_service_input_model)
+    def post(self):
         """ 
         Contructs the JSON structure that needs to be added to the Issuer's DID Document Service Endpoint Section. 
 
@@ -90,11 +141,12 @@ class EIDASServiceEndpoint(Resource):
         Section of the Issuer's DID Document.
         """
 
-        output = eidas_get_service_endpoint(
-                service_endpoints[0][0], 
-                service_endpoints[0][1]
+        return json.loads(
+            eidas_get_service_endpoint(
+                request.json['did'], 
+                request.json['service_endpoint']
             )
-        return jsonify(json.loads(output))
+        )
 
 @eidas.route('/sign-credential')
 class EIDASSignCredential(Resource):
