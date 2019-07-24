6c0123e9d212a5e55b4819faaa71b286fa76b6e3
Albert Solana <albertsolana@MacBook-de-Clara.local>
2019-07-24 13:33:19 +0200

    serving correctly website files

--- demo/util/backend_server.py
+++ demo/util/backend_server.py
@@ -1,14 +1,14 @@
 import http.server
 import socketserver
+import os
 
-DIRECTORY = "demo/university_backend"
+DIRECTORY = "../university_backend/"
 
+web_dir = os.path.join(os.path.dirname(__file__), DIRECTORY)
+os.chdir(web_dir)
 
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, directory=DIRECTORY, **kwargs)
-
-def start_backend_server(host='0.0.0.0', port='8080'):
-    with socketserver.TCPServer((host, port), Handler) as httpd:
-        print(' * Starting web backend server at http://' + host + ':' + str(port))
-        httpd.serve_forever()
+def start_backend_server(host='0.0.0.0', port=8080):
+    Handler = http.server.SimpleHTTPRequestHandler
+    httpd = socketserver.TCPServer((host, port), Handler)
+    print(' * Starting web backend server at http://' + host + ':' + str(port))
+    httpd.serve_forever()
