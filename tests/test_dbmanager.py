8090b6c04d3bf127abf49a2d09d4f2506c6ffbbd
Albert Solana <albertsolana@MacBook-de-Clara.local>
2019-09-17 14:45:06 +0200

    new test

--- tests/test_dbmanager.py
+++ tests/test_dbmanager.py
@@ -2,6 +2,7 @@
 
 import pytest, os
 from eidas_bridge.utils.dbmanager import DBManager, EIDASNotDataCreated
+from demo.data.common_data import all_type_dids
 
 def test_DBManager_class():
     dbmanager = DBManager()
@@ -15,5 +16,15 @@ def test_DBManager_path():
 
     assert dbmanager._file_path == new_path_file
     assert os.path.exists(dbmanager._file_path)
+
+    os.remove(new_path_file)
+
+@pytest.mark.parametrize("did", all_type_dids)
+def test_get_did_data_error(did):
+    new_path_file = "./demo/data/eidas_data_error.csv"
+    dbmanager = DBManager(new_path_file)
+
+    with pytest.raises(EIDASNotDataCreated):
+        dbmanager._get_did_data(did)
 
     os.remove(new_path_file)
